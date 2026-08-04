fb608644fb1891c353e1ab28c89c6b1ec1f9d8fa
Parthasarathy <parthasarathybala509@gmail.com>
2026-08-04 13:36:39 +0530

    Time: 1565 ms (9.22%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,12 +1,14 @@
-select Department,Employee,Salary from
-(
-
-Select d.name Department,
-Salary ,
-d.id,
-e.name Employee ,
-dense_rank() over(partition by d.name order by Salary desc) as D_rank
-from Employee e inner join Department 
-d on e.departmentId=d.id  ) as c 
-where D_rank =1 or D_rank =2 or D_rank =3
-;
+SELECT 
+    d.name AS Department,
+    c.Employee,
+    c.Salary
+FROM (
+    SELECT 
+        departmentId,
+        name AS Employee,
+        Salary,
+        DENSE_RANK() OVER(PARTITION BY departmentId ORDER BY Salary DESC) AS D_rank
+    FROM Employee
+) as c 
+INNER JOIN Department d ON c.departmentId = d.id
+WHERE c.D_rank <= 3;
