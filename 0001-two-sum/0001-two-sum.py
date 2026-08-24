7a0d9ccea5c0126918e54d72729f12ede9456692
Parthasarathy <parthasarathybala509@gmail.com>
2026-08-24 18:53:49 +0530

    Time: 0 ms (100%), Space: 12.9 MB (95.32%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}  
+        for index, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], index]
+            seen[num] = index
