36e8e7e5079d356685b8e2bfa5d2dcc464bd6064
Parthasarathy <parthasarathybala509@gmail.com>
2026-08-26 00:32:58 +0530

    Time: 556 ms (81.87%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,11 +1,10 @@
 select 
-Round(count(distinct player_id)/(select count(distinct player_id)from activity),2) 
- as fraction from
- (select player_id,
- device_id,
- event_date,
- lead(event_date,1) over(partition by player_id order by event_Date asc) as next_login,
- min(event_date) over(partition by player_id ) as first_login from Activity
- ) as c 
- where datediff(next_login,event_date)=1
- and event_date =first_login ;
+Round(count(player_id)/(select count(distinct player_id) from Activity),2) as fraction
+from
+(select 
+player_id,event_date,
+min(event_date) over(partition by player_id)as first_login,
+lead(event_date ,1 ) over(partition by player_id order by event_date asc) as next_login
+ from 
+Activity)as c 
+where datediff(next_login,first_login)=1;
